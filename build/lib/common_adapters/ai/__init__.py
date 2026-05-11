1936de3df0c2920aaab793fbd28f1137e3a10a25
Ashish-Coforge2 <ashish.2@coforge.com>
2026-05-11 13:38:23 +0530

    fix for packages not being installed

--- build/lib/common_adapters/ai/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import LLMAdapter
-from .openai import OpenAIAdapter
-from .unified import UnifiedAIAdapter
-from .config import AzureConfig, AWSConfig
-from .factory import get_unified_adapter
--- build/lib/common_adapters/ai/base.py
+++
@@ -1,7 +0,0 @@
-
-from abc import ABC, abstractmethod
-
-class LLMAdapter(ABC):
-    @abstractmethod
-    def generate(self, prompt: str) -> str:
-        raise NotImplementedError
