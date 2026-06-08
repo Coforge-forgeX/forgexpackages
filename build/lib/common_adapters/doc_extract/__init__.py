a1ee38d3dda2c22863d30c3eb920d9f2d704fa7a
Pratham Aggarwal <Pratham.Aggarwal@Coforge.com>
2026-06-08 16:20:11 +0530

    chore: remove test files and build artifacts

--- build/lib/common_adapters/doc_extract/__init__.py
+++
@@ -1,3 +0,0 @@
-# doc_extract/__init__.py
-from .base import DocumentExtractor, ExtractResult
-from .reader import DocReader
--- build/lib/common_adapters/download_response/__init__.py
+++
@@ -1,7 +0,0 @@
-from .download import download_message
-from .prompts import download_cleanup
-
-__all__ = [
-    "download_message",
-    "download_cleanup"
-]
